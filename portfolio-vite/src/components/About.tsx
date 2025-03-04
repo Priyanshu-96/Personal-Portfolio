d2dafbd27e290b21a866d261c4475ae9788b338d
Priyanshu-96 <priyanshusrivastava9696@gmail.com>
2025-03-04 22:37:46 +0530

    Small updates in About.tsx

--- portfolio-vite/src/components/About.tsx
+++ portfolio-vite/src/components/About.tsx
@@ -2,10 +2,10 @@ import SkillsMarquee from "./SkillsMarquee";
 
 const About = () => {
   return (
-    <section className="bg-black text-white py-20 relative">
+    <section className="bg-white text-black py-20 relative">
       <div className="container mx-auto px-6">
         <h2 className="text-4xl font-bold mb-6">ABOUT ME</h2>
-        <p className="text-gray-300 leading-relaxed max-w-2xl">
+        <p className="text-slate-500 leading-relaxed ">
           I'm a full-stack software engineer with a primary focus on front-end development.
           I love working at the intersection of creativity and technology.
           My expertise lies in JavaScript, React, and building user-friendly web experiences.
