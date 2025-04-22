13e9adf7783493923ebf2f68a936349572be54e1
Priyanshu-96 <priyanshusrivastava9696@gmail.com>
2025-04-23 02:44:45 +0530

    added skills.ts where all the skills are stored

---
+++ portfolio-vite/src/data/skills.ts
@@ -0,0 +1,16 @@
+// src/data/skills.ts
+export const skillsData = [
+    { name: "HTML", icon: "devicon-html5-plain colored" },
+    { name: "CSS", icon: "devicon-css3-plain colored" },
+    { name: "JavaScript", icon: "devicon-javascript-plain colored" },
+    { name: "React", icon: "devicon-react-original colored" },
+    { name: "Next.js", icon: "devicon-nextjs-original-wordmark colored" },
+    { name: "Tailwind CSS", icon: "devicon-tailwindcss-plain colored" },
+    { name: "Node.js", icon: "devicon-nodejs-plain colored" },
+    { name: "Express", icon: "devicon-express-original" },
+    { name: "MongoDB", icon: "devicon-mongodb-plain colored" },
+    { name: "Git", icon: "devicon-git-plain colored" },
+    { name: "Figma", icon: "devicon-figma-plain colored" },
+    { name: "Framer Motion", icon: "devicon-react-original colored" },
+  ];
+  
